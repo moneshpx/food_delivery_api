d0b92b91b5f0749c0b0877d4a6e9458f14ee56fa
moneshpx <monesh.s@pixoatic.com>
2024-03-27 11:34:55 +0530

    add the categories rspec

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -8,5 +8,18 @@ RSpec.describe "Categories", type: :request do
       category = JSON.parse(response.body)
       expect(category.first["name"]).to eq("pizza")
     end
+
+    it("Search the Categories by name") do
+      category = Category.create(name: "Pizza",image: "fjdkfdfdd")
+      get '/categories/search',params:{query: "pizza"}
+      category = JSON.parse(response.body)
+      expect(category.first["name"]).to eq("pizza")
+    end
+
+    it("If category not found")do
+      get '/categories/search',params:{query: "burger"}
+      category = JSON.parse(response.body)
+      expect(category["message"]).to eq("Categories not found")
+    end
   end
 end
